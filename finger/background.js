eb5b58586b52b2387cb4ace5480695b7a17d4d33
Michelle Bu <analogmidnight@gmail.com>
2012-10-13 04:54:31 -0700

    regex working

--- finger/background.js
+++ finger/background.js
@@ -4,6 +4,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   	chrome.windows.create({'url': 'http://localhost:8000/camera.html', 'type': 'popup', 'width': 600, 'height': 200}, function(window) {
 	});
 	
+	// match regex for next page.
+	var nextpagefunc = "document.querySelector('#next, .next, .more, #more').click();"
 	
 	chrome.extension.onConnect.addListener(function(port) {
 	  console.assert(port.name == "activate");
@@ -15,8 +17,20 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 						console.log('go back', tab[0].id);
 					});
 				} else if (msg.command == 'forward') {
+					var url = tab[0].url;
 					chrome.tabs.executeScript(tab[0].id, {code: "history.go(1);"}, function() {
 						console.log('go forward', tab[0].id);
+						chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function(tab2) {
+							if (tab2[0]) {
+								url2 = tab2[0].url;
+								if (url2 == url) {
+									console.log(url, url2);
+									chrome.tabs.executeScript(tab2[0].id, {code: nextpagefunc}, function() {
+										console.log('next page');
+									});
+								}
+							}
+						})
 					});
 				}
 			}
