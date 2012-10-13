2be74f51b47f36fc9fb30bff00ed5cf3f9b831bd
Michelle Bu <analogmidnight@gmail.com>
2012-10-13 06:22:04 -0700

    topdown array works

--- finger/evil.js
+++ finger/evil.js
@@ -1,18 +1,9 @@
 $(function() {
-	console.log('content script injected2');
-	console.log(window);
-	
 	var port = chrome.extension.connect({name: "activate"});
 	
 	function doSomething(msg) {
 		port.postMessage({command: msg});
-		console.log('posted');
 	}
-	
-	
-	port.onMessage.addListener(function(msg) {
-		// dunno
-	});
 	
 	// TODO: Q-learning, says Hai
 	var WIDTH = 40;
@@ -21,14 +12,15 @@ $(function() {
 	var YSECTIONS = 4;
 	var SAMPLES = 5;
 	var THRESHOLD = 4;
+	var DELAY = 50;
+	
 	var video = document.querySelector('video');
 	var canvas = document.querySelector('#cam');
 	var gridcanvas = document.querySelector('#grid');
 	var ctx = canvas.getContext('2d');
 	var grid = gridcanvas.getContext('2d');
+	
 	var localMediaStream = null;
-	//var imageData = ctx.createImageData(400, 300);
-	//var tracker = new HT.Tracker();
 	var xsections, xprevsections, ysections, yprevsections;
 	var inarow = 0;
 	
@@ -58,11 +50,11 @@ $(function() {
 		    grid.stroke();
 		}
 	}
+	
 	function snapshot() {
 		// TODO: add buffer between sections to allow for some error.
 	  if (localMediaStream) {
 		ctx.drawImage(video, 0, 0, WIDTH, HEIGHT);
-		
 		
 		drawLines();
 		if (it % SAMPLES == 0) {
@@ -72,6 +64,7 @@ $(function() {
 			var xsections = [];
 			var ysections = [];
 			var xdifferences = [];
+			
 			for (var i = 0; i < XSECTIONS; i++) {
 				xsections[i] = 0;
 				xdifferences[i] = 0;
@@ -82,20 +75,27 @@ $(function() {
 			for (var i = 0; i < pixels.length; i += 4) {
 				xsections[(i/4) % XSECTIONS] += pixels[i];
 			}
+			var yiter = 0;
 			for (var i = 0; i < pixels.length; i += 4) {
-				ysections[0] += pixels[i];
-				if (i >= pixels.length/YSECTIONS) {
-					break;
+				ysections[yiter] += pixels[i];
+				if (i != 0 && i % (pixels.length/YSECTIONS) == 0) {
+					yiter++;
 				}
 			}
+			
 			// take avg and find all diffs
 			var lowdiffs = true;
-				ysections[0] = Math.round(ysections[0] / ((WIDTH * HEIGHT) / YSECTIONS));
-				if (yprevsections) {
-					if (Math.abs(yprevsections[0] - ysections[0]) > 1) {
-						lowdiffs = false;
-					}
+			
+			// check that the top is unmoving.
+			for (var i in ysections) {
+				ysections[i] = Math.round(ysections[i] / ((WIDTH * HEIGHT) / YSECTIONS));
+			}
+				
+			if (yprevsections) {
+				if (Math.abs(yprevsections[0] - ysections[0]) > 1) {
+					lowdiffs = false;
 				}
+			}
 			for (var i in xsections) {
 				xsections[i] = /*Math.round(*/xsections[i] / ((WIDTH * HEIGHT) / XSECTIONS)/*)*/;
 				if (xprevsections) {
@@ -107,13 +107,14 @@ $(function() {
 			}
 			
 			// TODO: fix this 'heuristic'
+			// heuristic for left-right.
 			if (xprevsections && 
 				lowdiffs &&
 				delay == 0 &&
 				Math.abs(xdifferences[0]) > THRESHOLD &&
 				Math.abs(xdifferences[3]) > THRESHOLD &&
 				Math.abs(Math.abs(xdifferences[0]) - Math.abs(xdifferences[3])) < 2) {
-				console.log(xsections, xprevsections, xdifferences, (xsections[0]+xsections[1]+xsections[2]+xsections[3])/4);
+				console.log(xsections, xprevsections, xdifferences, ysections);
 				inarow ++;
 				if (xdifferences[0] < 0 && xdifferences[3] < 0) {
 					action = 'back';
@@ -126,33 +127,28 @@ $(function() {
 					
 					// this should be fine unless the background is extremely red or something.
 					doSomething(action);
-					delay = 50;
+					delay = DELAY;
 				} else if (inarow > 0) {
 					console.log('too much movement!')
-					delay = 50;
+					delay = DELAY;
 				}
 				inarow = 0;
 			}
+			
+			// heuristic for top-bottom.
+			
+			
+			
 			xprevsections = xsections;
 			yprevsections = ysections;
 		}
 		it++;
-		
-		/*imageData = ctx.getImageData(0, 0, 400, 300);
-		if (it % 1 == 0) {
-			candidate = tracker.detect(imageData);
-		}
-		if (candidate) {
-			drawHull(candidate.hull, "red");
-		}
-		//console.log(candidate.hull);
-		it++;
-		*/
 	  }
 	
 	  setTimeout(function() {
 		window.webkitRequestAnimationFrame(snapshot);
 	  }, 0);
+	
 	  if (delay > 0) {
 		delay--;
 		if (delay == 0) {
@@ -161,32 +157,11 @@ $(function() {
 	  }
 	}
 	
-	/*function drawHull(hull, color) {
-	      var len = hull.length, i = 1;
-
-	      if (len > 0){
-	        ctx.beginPath();
-	        ctx.lineWidth = 3;
-	        ctx.strokeStyle = color;
-
-	        ctx.moveTo(hull[0].x, hull[0].y);
-	        for (; i < len; ++ i){
-	          ctx.lineTo(hull[i].x, hull[i].y);
-	        }
-
-	        ctx.stroke();
-	        ctx.closePath();
-	      }
-	};*/
-	
-
-	
 	window.webkitRequestAnimationFrame(snapshot);
 	
 
-	// Not showing vendor prefixes or code that works cross-browser.
 	navigator.webkitGetUserMedia({video: true}, function(stream) {
 	  video.src = window.webkitURL.createObjectURL(stream);
 	  localMediaStream = stream;
-	}, function() {console.log('poop');});
+	}, function() {});
 });
